9710e5f5814c4c9a5363631483fb3a748834b05a
Ramon van Alteren <ramon@vanalteren.nl>
2012-01-02 15:00:51 +0100

    Fixing up setup.py refs

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 GLOBAL_ENTRY_POINTS = {
-        "console_scripts":[ "jenkins_invoke=pyhudson.command_line.hudson_invoke:main",
-                            "meta_test=pyhudson.command_line.meta_test:main", ] }
+        "console_scripts":[ "jenkins_invoke=pyjenkinsci.command_line.hudson_invoke:main",
+                            "meta_test=pyjenkinsci.command_line.meta_test:main", ] }
 
 setup(name='pyjenkinsci',
-      version='0.0.35',
-      description='A Python API for accessing resources a Hudson or Jenkins continuous-integration server.',
+      version='0.0.35.1',
+      description='A Python API for accessing resources on a Jenkins continuous-integration server.',
       author='Salim Fadhley',
       author_email='sal@stodge.org',
-      #install_requires = [ 'elementtree>=1.2-20040618' ],
-      #tests = "tests",
-      package_dir = {'':'src'},
-      packages=find_packages('src'),
+      package_dir = {'':'pyjenkinsci'},
+      packages=find_packages('pyjenkinsci'),
       zip_safe=True,
       include_package_data = False,
       entry_points = GLOBAL_ENTRY_POINTS,
+      url="https://github.com/ramonvanalteren/pyjenkinsci",
       )
