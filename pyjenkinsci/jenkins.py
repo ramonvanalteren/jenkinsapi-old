32ae55ef135a94de445dfb761c223bbbb5db1c4c
Ramon van Alteren <ramon@vanalteren.nl>
2012-01-03 17:47:22 +0100

    Added authentification and node classes
    In order to add nodes, I need to be able to authenticate myself against jenkins
    Added additional code to the opener to allow authentication using a custom AuthHandler
    Jenkins uses BasicAuth without a challenge, so I had to implement a small subclass which I pulled from stackoverflow.
    
    Node class + methods on Jenkins class added
    Ability to add and remove nodes, check for presence and basic data

--- pyjenkinsci/jenkins.py
+++ pyjenkinsci/jenkins.py
@@ -7,6 +7,9 @@ from pyjenkinsci.exceptions import UnknownJob
 from utils.urlopener import mkurlopener
 import logging
 import time
+import urllib2
+import urllib
+import json
 
 log = logging.getLogger(__name__)
 
@@ -14,7 +17,20 @@ class Jenkins(JenkinsBase):
     """
     Represents a jenkins environment.
     """
-    def __init__(self, baseurl, proxyhost=None, proxyport=None, proxyuser=None, proxypass=None):
+    def __init__(self, baseurl, username=None, password=None, proxyhost=None, proxyport=None, proxyuser=None, proxypass=None):
+        """
+
+        :param baseurl: baseurl for jenkins instance including port, str
+        :param username: username for jenkins auth, str
+        :param password: password for jenkins auth, str
+        :param proxyhost: proxyhostname, str
+        :param proxyport: proxyport, int
+        :param proxyuser: proxyusername for proxy auth, str
+        :param proxypass: proxypassword for proxyauth, str
+        :return: a Jenkins obj
+        """
+        self.username = username
+        self.password = password
         self.proxyhost = proxyhost
         self.proxyport = proxyport
         self.proxyuser = proxyuser
@@ -22,10 +38,21 @@ class Jenkins(JenkinsBase):
         JenkinsBase.__init__( self, baseurl )
 
     def get_proxy_auth(self):
-        return (self.proxyhost, self.proxyport, self.proxyuser, self.proxypass)
+        return self.proxyhost, self.proxyport, self.proxyuser, self.proxypass
+
+    def get_jenkins_auth(self):
+        return self.username, self.password, self.baseurl
+
+    def get_auth(self):
+        auth_args = []
+        auth_args.extend(self.get_jenkins_auth())
+        auth_args.extend(self.get_proxy_auth())
+        log.debug("args: %s" % auth_args)
+        return auth_args
+
 
     def get_opener( self ):
-        return mkurlopener(*self.get_proxy_auth())
+        return mkurlopener(*self.get_auth())
 
     def validate_fingerprint( self, id ):
         obj_fingerprint = Fingerprint(self.baseurl, id, jenkins_obj=self)
@@ -99,7 +126,10 @@ class Jenkins(JenkinsBase):
     def get_node_dict(self):
         """Get registered slave nodes on this instance"""
         url = self.python_api_url(self.get_node_url())
-        return dict(self.get_data(url))
+        node_dict = dict(self.get_data(url))
+        return dict(
+            (node['displayName'], self.python_api_url(self.get_node_url(node['displayName'])))
+                for node in node_dict['computer'])
 
     def get_node(self, nodename):
         """Get a node object for a specific node"""
@@ -110,3 +140,81 @@ class Jenkins(JenkinsBase):
         """Return the url for nodes"""
         url = "%(baseurl)s/computer/%(nodename)s" % {'baseurl': self.baseurl, 'nodename': nodename}
         return url
+
+    def has_node(self, nodename):
+        """
+        Does a node by the name specified exist
+        :param nodename: string, hostname
+        :return: boolean
+        """
+        return nodename in self.get_node_dict()
+
+    def delete_node(self, nodename):
+        """
+        Remove a node from the managed slave list
+        Please note that you cannot remove the master node
+
+        :param nodename: string holding a hostname
+        :return: None
+        """
+        assert self.has_node(nodename), "This node: %s is not registered as a slave" % nodename
+        assert nodename != "master", "you cannot delete the master node"
+        url = "%s/doDelete" % self.get_node_url(nodename)
+        fn_urlopen = self.get_jenkins_obj().get_opener()
+        try:
+            stream = fn_urlopen(url)
+            html_result = stream.read()
+        except urllib2.HTTPError, e:
+            log.debug("Error reading %s" % url)
+            raise
+        return not self.has_node(nodename)
+
+    def create_node(self, name, num_executors=2, node_description=None,
+                    remote_fs='/var/lib/jenkins', labels=None, exclusive=False):
+        """
+        Create a new slave node by name.
+
+        :param name: fqdn of slave, str
+        :param num_executors: number of executors, int
+        :param node_description: a freetext field describing the node
+        :param remote_fs: jenkins path, str
+        :param labels: labels to associate with slave, str
+        :param exclusive: tied to specific job, boolean
+        :return: node obj
+        """
+        NODE_TYPE   = 'hudson.slaves.DumbSlave$DescriptorImpl'
+        MODE = 'NORMAL'
+        if self.has_node(name):
+            return Node(nodename=name, baseurl=self.get_node_url(nodename=name), jenkins_obj=self)
+        if exclusive:
+            MODE = 'EXCLUSIVE'
+        params = {
+            'name' : name,
+            'type' : NODE_TYPE,
+            'json' : json.dumps ({
+                'name'            : name,
+                'nodeDescription' : node_description,
+                'numExecutors'    : num_executors,
+                'remoteFS'        : remote_fs,
+                'labelString'     : labels,
+                'mode'            : MODE,
+                'type'            : NODE_TYPE,
+                'retentionStrategy' : { 'stapler-class'  : 'hudson.slaves.RetentionStrategy$Always' },
+                'nodeProperties'    : { 'stapler-class-bag' : 'true' },
+                'launcher'          : { 'stapler-class' : 'hudson.slaves.JNLPLauncher' }
+            })
+        }
+        url = "%(nodeurl)s/doCreateItem?%(params)s" % {
+            'nodeurl': self.get_node_url(),
+            'params': urllib.urlencode(params)
+        }
+        print url
+        fn_urlopen = self.get_jenkins_obj().get_opener()
+        try:
+            stream = fn_urlopen(url)
+            html_result = stream.read()
+        except urllib2.HTTPError, e:
+            log.debug("Error reading %s" % url)
+            log.exception(e)
+            raise
+        return Node(nodename=name, baseurl=self.get_node_url(nodename=name), jenkins_obj=self)
--- pyjenkinsci/node.py
+++ pyjenkinsci/node.py
@@ -29,105 +29,15 @@ class Node(JenkinsBase):
     def __str__(self):
         return self.id()
 
+    def get_node_data(self):
+        return self._data
+
     def is_online(self):
         return not self._data['offline']
 
-#NODES       = 'computer/api/json'
-#CREATE_NODE = 'computer/doCreateItem?%s'
-#DELETE_NODE = 'computer/%(name)s/doDelete'
-#NODE_INFO   = 'computer/%(name)s/api/json?depth=0'
-#NODE_TYPE   = 'hudson.slaves.DumbSlave$DescriptorImpl'
-#
-#def get_nodes(self):
-#    '''
-#    Get a list of nodes registered on the jenkins instance
-#    returns a list of node dictionaries
-#    '''
-#    try:
-#        response = self.jenkins_open(urllib2.Request(self.server + NODES%locals()))
-#        if response:
-#            return json.loads(response)['computer']
-#        else:
-#            raise JenkinsException('Cannot get nodes information')
-#    except urllib2.HTTPError:
-#        raise JenkinsException('Cannot get nodes information')
-#    except ValueError:
-#        raise JenkinsException("Could not parse JSON info for nodes information")
-#
-#def get_node_info(self, name):
-#    '''
-#    Get node information dictionary
-#
-#    :param name: Node name, ``str``
-#    :returns: Dictionary of node info, ``dict``
-#    '''
-#    try:
-#        response = self.jenkins_open(urllib2.Request(self.server + NODE_INFO%locals()))
-#        if response:
-#            return json.loads(response)
-#        else:
-#            raise JenkinsException('node[%s] does not exist'%name)
-#    except urllib2.HTTPError:
-#        raise JenkinsException('node[%s] does not exist'%name)
-#    except ValueError:
-#        raise JenkinsException("Could not parse JSON info for node[%s]"%name)
-#
-#def node_exists(self, name):
-#    '''
-#    :param name: Name of Jenkins node, ``str``
-#    :returns: ``True`` if Jenkins node exists
-#    '''
-#    try:
-#        self.get_node_info(name)
-#        return True
-#    except JenkinsException:
-#        return False
-#
-#def delete_node(self, name):
-#    '''
-#    Delete Jenkins node permanently.
-#
-#    :param name: Name of Jenkins node, ``str``
-#    '''
-#    self.get_node_info(name)
-#    self.jenkins_open(urllib2.Request(self.server + DELETE_NODE%locals(), ''))
-#    if self.node_exists(name):
-#        raise JenkinsException('delete[%s] failed'%(name))
-#
-#def create_node(self, name, numExecutors=2, nodeDescription=None,
-#                remoteFS='/var/lib/jenkins', labels=None, exclusive=False):
-#    '''
-#    :param name: name of node to create, ``str``
-#    :param numExecutors: number of executors for node, ``int``
-#    :param nodeDescription: Description of node, ``str``
-#    :param remoteFS: Remote filesystem location to use, ``str``
-#    :param labels: Labels to associate with node, ``str``
-#    :param exclusive: Use this node for tied jobs only, ``bool``
-#    '''
-#    if self.node_exists(name):
-#        raise JenkinsException('node[%s] already exists'%(name))
-#
-#    mode = 'NORMAL'
-#    if exclusive:
-#        mode = 'EXCLUSIVE'
-#
-#    params = {
-#        'name' : name,
-#        'type' : NODE_TYPE,
-#        'json' : json.dumps ({
-#            'name'            : name,
-#            'nodeDescription' : nodeDescription,
-#            'numExecutors'    : numExecutors,
-#            'remoteFS'        : remoteFS,
-#            'labelString'     : labels,
-#            'mode'            : mode,
-#            'type'            : NODE_TYPE,
-#            'retentionStrategy' : { 'stapler-class'  : 'hudson.slaves.RetentionStrategy$Always' },
-#            'nodeProperties'    : { 'stapler-class-bag' : 'true' },
-#            'launcher'          : { 'stapler-class' : 'hudson.slaves.JNLPLauncher' }
-#        })
-#    }
-#
-#    self.jenkins_open(urllib2.Request(self.server + CREATE_NODE%urllib.urlencode(params)))
-#    if not self.node_exists(name):
-#        raise JenkinsException('create[%s] failed'%(name))
+    def is_jnlpagent(self):
+        return self._data['jnlpAgent']
+
+    def is_idle(self):
+        return self._data['idle']
+
