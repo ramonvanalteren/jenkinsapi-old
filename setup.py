76684dccd384a7196dcbbc497c7e515a4a9cdd36
Salim Fadhley <sal@stodge.org>
2012-01-08 13:36:00 +0000

    More tweaks.

--- setup.py
+++ setup.py
@@ -4,16 +4,19 @@ import sys, os
 # Use the README file as the long-description for the project. 
 # This makes sure that the PyPi index page contains automatically
 # generated documentation.
-PROJECT_ROOT, _ = os.path.split(sys.argv[0]) 
+PROJECT_ROOT, _ = os.path.split(__file__) 
 DESCRIPTION = open( os.path.join(PROJECT_ROOT, "README") ).read()
+VERSION = REVISION = '0.1.1'
+PROJECT_NAME = 'jenkinsapi'
+PROJECT_AUTHORS = "Salim Fadhley, Ramon van Alteren"
 
 GLOBAL_ENTRY_POINTS = {
         "console_scripts":[ "jenkins_invoke=jenkinsapi.command_line.jenkins_invoke:main"]
         }
 
-setup(name='jenkinsapi',
-      version='0.1',
-      author="Salim Fadhley, Ramon van Alteren",
+setup(name=PROJECT_NAME,
+      version=VERSION,
+      author=PROJECT_AUTHORS,
       author_email='salimfadhley@gmail.com, ramon@vanalteren.nl',
       packages=find_packages('.'),
       zip_safe=True,
