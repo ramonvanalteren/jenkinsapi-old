5c7f0515f4263cec7057932d83896a9f51c1f7ba
Ramon van Alteren <ramon@vanalteren.nl>
2012-02-08 11:12:43 +0100

    Cleaned up whitespace in examples

--- examples/search_artifact_by_regexp.py
+++ examples/search_artifact_by_regexp.py
@@ -1,11 +1,8 @@
 from jenkinsapi.api import search_artifact_by_regexp
-import logging
 import re
 
 jenkinsurl = "http://localhost:8080/jenkins"
 jobid = "test1"
-artifactRegExp = re.compile( "test1\.txt" ) # A file name I want.
-
-result = search_artifact_by_regexp( jenkinsurl, jobid, artifactRegExp )
-
-print repr( result )
+artifact_regexp = re.compile("test1\.txt") # A file name I want.
+result = search_artifact_by_regexp(jenkinsurl, jobid, artifact_regexp)
+print repr(result)
