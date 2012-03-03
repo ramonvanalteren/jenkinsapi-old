8145850506ba238682f189403f4545183db9bec7
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-03-03 10:48:38 -0300

    Fix pep8 and open file description

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 import os
 
-PROJECT_ROOT, _ = os.path.split(__file__) 
-DESCRIPTION = open( os.path.join(PROJECT_ROOT, "README") ).read()
+PROJECT_ROOT, _ = os.path.split(__file__)
+DESCRIPTION = open(os.path.join(PROJECT_ROOT, "README.rst")).read()
 VERSION = REVISION = '0.1.4'
 PROJECT_NAME = 'JenkinsAPI'
 PROJECT_AUTHORS = "Salim Fadhley, Ramon van Alteren, Ruslan Lutsenko"
@@ -11,7 +11,7 @@ PROJECT_URL = "https://github.com/salimfadhley/jenkinsapi"
 SHORT_DESCRIPTION = 'A Python API for accessing resources on a Jenkins continuous-integration server.'
 
 GLOBAL_ENTRY_POINTS = {
-        "console_scripts":[ "jenkins_invoke=jenkinsapi.command_line.jenkins_invoke:main"]
+        "console_scripts": ["jenkins_invoke=jenkinsapi.command_line.jenkins_invoke:main"]
         }
 
 # Actual setup
@@ -20,10 +20,10 @@ setup(name=PROJECT_NAME.lower(),
       version=VERSION,
       author=PROJECT_AUTHORS,
       author_email=PROJECT_EMAILS,
-      packages=["jenkinsapi",'jenkinsapi.utils','jenkinsapi.command_line'], 
+      packages=["jenkinsapi", 'jenkinsapi.utils', 'jenkinsapi.command_line'],
       zip_safe=True,
-      include_package_data = False,
-      entry_points = GLOBAL_ENTRY_POINTS,
+      include_package_data=False,
+      entry_points=GLOBAL_ENTRY_POINTS,
       url=PROJECT_URL,
       description=SHORT_DESCRIPTION,
       long_description=DESCRIPTION,
