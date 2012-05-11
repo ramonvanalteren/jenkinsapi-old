87d37c8d3e94da9cb3a9d001e7d088df32fba9dc
William Zhang <jollychang@gmail.com>
2012-05-11 11:46:52 +0800

    add try expect

--- jenkinsapi/jenkinsbase.py
+++ jenkinsapi/jenkinsbase.py
@@ -73,8 +73,12 @@ class JenkinsBase(object):
         return result
 
     def post_data(self, url, content):
-        request = urllib2.Request(url, content)
-        result = urllib2.urlopen(request).read().strip()
+        try:
+            request = urllib2.Request(url, content)
+            result = urllib2.urlopen(request).read().strip()
+        except urllib2.HTTPError, e:
+            log.warn("Error post data %s" % url)
+            log.exception(e)
         return result
 
     def hit_url(self, url ):
