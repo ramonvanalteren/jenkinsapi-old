1e3dec59b20e03abcdb77900b4db8f39d1d1f17d
salimfadhley <sal@stodge.org>
2013-06-12 23:59:28 +0100

    ci should prefer nosetests

--- setup.py
+++ setup.py
@@ -35,4 +35,5 @@ setup(name=PROJECT_NAME.lower(),
       url=PROJECT_URL,
       description=SHORT_DESCRIPTION,
       long_description=DESCRIPTION,
+      use_2to3 = True, # Experimental support for Python 3
       )
