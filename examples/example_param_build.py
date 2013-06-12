c5f72fdd076d02e820d4d2080dcf4922f7a0a8fc
salimfadhley <sal@stodge.org>
2013-06-12 01:59:01 +0100

    a lower-level example of how parameterized builds work in Jenkins

---
+++ examples/example_param_build.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+def foo():
+    """
+    A low level example of how to run a parameterized build in Jenkins
+    from Python.
+    """
+    data={'B':'abc'}
+    toJson = {'parameter':[{'name':'B', 'value':'xyz'}]}
+    url = 'http://localhost:8080/job/ddd/build'
+    #url = 'http://localhost:8000'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    form = {'json':json.dumps(toJson)}
+    response = requests.post(url, data=form, headers=headers)
+    print response.text.encode('UTF-8')
+
+if __name__ == '__main__':
+    foo()
+
