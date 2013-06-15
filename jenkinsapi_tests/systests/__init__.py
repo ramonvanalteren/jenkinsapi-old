6cb94ba02cc320d562f634062526e9ccfe6fcc05
salimfadhley <sal@stodge.org>
2013-06-15 22:33:06 +0100

    Get rid of the global - it's not needed any more.

--- jenkinsapi_tests/systests/__init__.py
+++ jenkinsapi_tests/systests/__init__.py
@@ -1,18 +1,12 @@
 import os
 from jenkinsapi_utils.jenkins_launcher import JenkinsLancher
 
-
-launcher = None
-
+systests_dir, _ = os.path.split(__file__)
+war_path = os.path.join(systests_dir, 'jenkins.war' )
+launcher = JenkinsLancher(war_path)
 
 def setUpPackage():
-    global launcher
-
-    systests_dir, _ = os.path.split(__file__)
-    war_path = os.path.join(systests_dir, 'jenkins.war' )
-    launcher = JenkinsLancher(war_path)
     launcher.start()
-
 
 def tearDownPackage():
     launcher.stop()
