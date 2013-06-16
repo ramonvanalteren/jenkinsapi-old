0651cf295c40fe99d519d3038b36308d2c8bf253
Salim Fadhley <sal@stodge.org>
2013-06-17 00:37:28 +0100

    corrected one of the api-methods

--- jenkinsapi_tests/systests/test_views.py
+++ jenkinsapi_tests/systests/test_views.py
@@ -3,7 +3,9 @@ System tests for `jenkinsapi.jenkins` module.
 '''
 import logging
 import unittest
+
 from jenkinsapi.view import View
+from jenkinsapi.api import get_view_from_url
 from jenkinsapi_tests.systests.base import BaseSystemTest
 from jenkinsapi_tests.test_utils.random_strings import random_string
 
@@ -19,10 +21,15 @@ class TestViews(BaseSystemTest):
         self.assertIn(view_name, self.jenkins.views())
         self.assertIsInstance(v, View)
 
+        # Can we use the API comnvenience methods
+        v2 = get_view_from_url(v.baseurl)
+        self.assertEquals(v, v2)
+
     def test_create_and_delete_views(self):
         self._create_job()
         view1_name = random_string()
         new_view = self.jenkins.views().create(view1_name)
+        self.assertIsInstance(new_view, View)
         self.assertIn(view1_name, self.jenkins.views())
         del self.jenkins.views()[view1_name]
         self.assertNotIn(view1_name, self.jenkins.views())
