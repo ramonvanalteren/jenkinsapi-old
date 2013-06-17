1db4c347b2cb2b1afb572ad8c42a4e02226df7e0
salimfadhley <sal@stodge.org>
2013-06-18 00:44:26 +0100

    yet another refactor of nodes, this time with better tests

--- jenkinsapi/nodes.py
+++ jenkinsapi/nodes.py
@@ -1,5 +1,6 @@
 import logging
 from jenkinsapi.node import Node
+from jenkinsapi.exceptions import UnknownNode
 from jenkinsapi.jenkinsbase import JenkinsBase
 
 log = logging.getLogger(__name__)
@@ -16,19 +17,36 @@ class Nodes(JenkinsBase):
         self.jenkins = jenkins_obj
         JenkinsBase.__init__(self, baseurl)
 
+    def get_jenkins_obj(self):
+        return self.jenkins
+
     def __str__(self):
         return 'Nodes @ %s' % self.baseurl
+
+    def __contains__(self, node_name):
+        return node_name in self.keys()
+
+    def iterkeys(self):
+        for item in self._data['computer']:
+            yield item['displayName']
+
+    def keys(self):
+        return list(self.iterkeys())
 
     def iteritems(self):
         for item in self._data['computer']:
             nodename = item['displayName']
-            if nodename == 'master':
+            if nodename.lower() == 'master':
                 nodeurl = '%s/(%s)' % (self.baseurl, nodename)
             else:
                 nodeurl = '%s/%s' % (self.baseurl, nodename)
-            yield item['displayName'], Node(nodeurl, nodename, self.jenkins)
+            try:
+                yield item['displayName'], Node(nodeurl, nodename, self.jenkins)
+            except Exception:
+                import ipdb; ipdb.set_trace()
 
     def __getitem__(self, nodename):
         for k, v in self.iteritems():
             if k == nodename:
                 return v
+        raise UnknownNode(nodename)
