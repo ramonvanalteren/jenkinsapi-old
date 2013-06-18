87d57df7ffcd04bf6d7fa31c1a0ad4e4cbd5f327
salimfadhley <sal@stodge.org>
2013-06-19 00:47:01 +0100

    more docs

---
+++ examples/how_to/get_version_info_from_last_good_build.py
@@ -0,0 +1,13 @@
+"""
+Extract version information from the latest build.
+"""
+from jenkinsapi.jenkins import Jenkins
+
+def getSCMInfroFromLatestGoodBuild(url, jobName, username=None, password=None):
+    J = Jenkins(url, username, password)
+    job = J[jobName]
+    lgb = job.get_last_good_build()
+    return lgb.get_revision()
+
+if __name__ == '__main__':
+    print getSCMInfroFromLatestGoodBuild('http://localhost:8080', 'fooJob')
