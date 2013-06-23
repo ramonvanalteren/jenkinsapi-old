ef44bb1a3de66c0274a997b6758d825c1a6e300b
Salim Fadhley <sal@stodge.org>
2013-06-23 09:26:01 +0100

    Moved the empty job config into the config

--- jenkinsapi_tests/systests/job_configs.py
+++ jenkinsapi_tests/systests/job_configs.py
@@ -1,3 +1,23 @@
+EMPTY_JOB = '''\
+<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class="hudson.scm.NullSCM"/>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers class="vector"/>
+  <concurrentBuild>false</concurrentBuild>
+  <builders/>
+  <publishers/>
+  <buildWrappers/>
+</project>
+'''.strip()
+
 LONG_RUNNING_JOB = JOB_XML = """
 <?xml version='1.0' encoding='UTF-8'?>
 <project>
