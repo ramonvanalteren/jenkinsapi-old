bc36243665cfeaf3d1be50208a222d49dad89a11
salimfadhley <sal@stodge.org>
2013-06-23 07:26:10 +0100

    Remove a redundant import

--- jenkinsapi/mutable_jenkins_thing.py
+++ jenkinsapi/mutable_jenkins_thing.py
@@ -1,7 +1,6 @@
-import urlparse
-
 class MutableJenkinsThing(object):
     """
+    A mixin for certain mutable objects which can be renamed and deleted.
     """
     def get_delete_url(self):
         return '%s/doDelete' % self.baseurl
