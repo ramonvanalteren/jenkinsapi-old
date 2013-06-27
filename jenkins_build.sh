4186a4c4c81dcb3dc1883e3e12558425ae5ada0d
salimfadhley <sal@stodge.org>
2013-06-27 22:29:44 +0100

    Added a script for all the jenkins activities.

---
+++ jenkins_build.sh
@@ -0,0 +1,8 @@
+virtualenv .
+source bin/activate
+python setup.py develop
+easy_install nose
+easy_install mock
+easy_install requests
+easy_install coverage
+nosetests jenkinsapi_tests --with-xunit --with-coverage --cover-html --cover-html-dir=$WORKSPACE/coverage_report --cover-package=jenkinsapi
