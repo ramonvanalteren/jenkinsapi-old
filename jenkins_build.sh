ca71b8777260e3335cf0f3e08900b4333b340052
Salim Fadhley <sal@stodge.org>
2013-06-30 00:17:40 +0100

    Make nosetests verbose

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -6,4 +6,4 @@ easy_install nose
 easy_install mock
 easy_install requests
 easy_install coverage
-nosetests jenkinsapi_tests --with-xunit --with-coverage --cover-html --cover-html-dir=$WORKSPACE/coverage_report --cover-package=jenkinsapi
+nosetests jenkinsapi_tests --with-xunit --with-coverage --cover-html --cover-html-dir=$WORKSPACE/coverage_report --cover-package=jenkinsapi --verbose
