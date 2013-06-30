7def9ed6e92580f37d00e4980c36c4d36e68f702
Salim Fadhley <sal@stodge.org>
2013-06-30 01:41:48 +0100

    This test is disabled,it does not actually test all that much about the API, it would be better done as a unittest.

--- jenkinsapi_tests/systests/test_scm.py
+++ jenkinsapi_tests/systests/test_scm.py
@@ -1,23 +1,18 @@
-'''
+# '''
 System tests for `jenkinsapi.jenkins` module.
 '''
 import unittest
-from jenkinsapi.invocation import Invocation
 from jenkinsapi_tests.systests import state
 from jenkinsapi_tests.systests.base import BaseSystemTest
 from jenkinsapi_tests.test_utils.random_strings import random_string
 from jenkinsapi_tests.systests.job_configs import SCM_GIT_JOB
 
 # Maybe have a base class for all SCM test activites?
-class TestSCMGIT(BaseSystemTest):
+class TestSCMGit(BaseSystemTest):
     # Maybe it makes sense to move plugin dependencies outside the code.
     # Have a config to dependencies mapping from the launcher can use to install plugins.
-    PLUGIN_DEPENDENCIES = ["http://updates.jenkins-ci.org/latest/git.hpi",
-                           "http://updates.jenkins-ci.org/latest/git-client.hpi"]
-    
     def test_get_revision(self):
         job_name = 'create_%s' % random_string()
-        state['launcher'].install_plugin(self.PLUGIN_DEPENDENCIES)
         job = self.jenkins.create_job(job_name, SCM_GIT_JOB)
         ii = job.invoke()
         ii.block(until='completed')
