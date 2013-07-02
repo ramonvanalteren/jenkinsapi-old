a9fb9aaf8668074e73863556f7a82bdc99f558bf
Aleksey Maksimov <ctpeko3a@gmail.com>
2013-07-02 18:14:38 +0800

    Added code to debug HTTP requests (commented)

--- jenkinsapi/utils/requester.py
+++ jenkinsapi/utils/requester.py
@@ -1,5 +1,18 @@
 import requests
 from jenkinsapi.exceptions import JenkinsAPIException
+# import logging
+
+# # these two lines enable debugging at httplib level (requests->urllib3->httplib)
+# # you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# # the only thing missing will be the response.body which is not logged.
+# import httplib
+# httplib.HTTPConnection.debuglevel = 1
+
+# logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
 
 
 class Requester(object):
