6547566581350107d75de41b08429d4c2e5e8e54
Christophe Bliard <christophe.bliard@smartesting.com>
2013-10-04 15:38:13 +0200

    ensure virtualenv installed and WORKSPACE defined

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+if ! command -v virtualenv >/dev/null 2>&1; then
+    echo "You should install virtualenv, check http://www.virtualenv.org/"
+    exit 1
+fi
 virtualenv .
 source bin/activate
 python setup.py develop
@@ -6,4 +10,5 @@ easy_install nose
 easy_install mock
 easy_install requests
 easy_install coverage
-nosetests jenkinsapi_tests --with-xunit --with-coverage --cover-html --cover-html-dir=$WORKSPACE/coverage_report --cover-package=jenkinsapi --verbose
+test -z "$WORKSPACE" && WORKSPACE="`pwd`"
+nosetests jenkinsapi_tests --with-xunit --with-coverage --cover-html --cover-html-dir="$WORKSPACE/coverage_report" --cover-package=jenkinsapi --verbose
