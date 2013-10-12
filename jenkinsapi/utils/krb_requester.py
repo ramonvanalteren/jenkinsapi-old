21bbaab1aa8c143ea3ec106be1d839e88512567c
salimfadhley <sal@stodge.org>
2013-10-12 18:35:41 +0100

    disable an annoying pylint warning

--- jenkinsapi/utils/krb_requester.py
+++ jenkinsapi/utils/krb_requester.py
@@ -4,7 +4,7 @@ Kerberos aware Requester
 from jenkinsapi.utils.requester import Requester
 from requests_kerberos import HTTPKerberosAuth, OPTIONAL
 
-
+#pylint: disable=W0222
 class KrbRequester(Requester):
     """
     A class which carries out HTTP requests with Kerberos/GSSAPI authentication.
