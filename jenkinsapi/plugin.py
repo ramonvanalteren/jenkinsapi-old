4d94424bd54c42cffac0069b971941142d4fa5aa
Aleksey Maksimov <ctpeko3a@gmail.com>
2014-01-04 02:28:24 +0800

    Fixed PyLint errors

--- jenkinsapi/plugin.py
+++ jenkinsapi/plugin.py
@@ -9,7 +9,7 @@ class Plugin(object):
     """
 
     def __init__(self, plugin_dict):
-        assert(isinstance(plugin_dict, dict))
+        assert isinstance(plugin_dict, dict)
         self.__dict__ = plugin_dict
 
     def __eq__(self, other):
