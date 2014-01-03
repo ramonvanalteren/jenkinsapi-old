287eda6125c910887ca9d549d987ca7ee006a6c7
Larry Cai <larry.caiyu@gmail.com>
2014-01-03 14:49:37 +0800

    update the create_view example to reflect the latest views API

--- examples/how_to/create_views.py
+++ examples/how_to/create_views.py
@@ -8,11 +8,14 @@ logging.basicConfig(level=log_level)
 logger = logging.getLogger()
 
 jenkins_url = "http://192.168.1.64:8080/"
+#jenkins_url = "http://localhost:7080/"
+
 api = Jenkins(jenkins_url)
 
 # Create ListView in main view
 logger.info('Attempting to create new view')
-new_view = api.create_view('SimpleListView')
+test_view_name = 'SimpleListView'
+new_view = api.views.create(test_view_name)
 logger.info('new_view is %s' % new_view)
 if new_view is None:
     logger.error('View was not created')
@@ -20,19 +23,22 @@ else:
     logger.info('View has been created')
 
 logger.info('Attempting to create view that already exists')
-if not api.create_view('SimpleListView'):
+if not api.views.create(test_view_name):
     logger.info('View was not created')
 else:
     logger.error('View has been created')
 
 logger.info('Attempting to delete view that already exists')
-if not api.delete_view('SimpleListView'):
+
+del api.views[test_view_name]
+if test_view_name in api.views:
     logger.error('View was not deleted')
 else:
     logger.info('View has been deleted')
 
 logger.info('Attempting to delete view that does not exist')
-if api.delete_view('SimpleListView'):
+del api.views[test_view_name]
+if not test_view_name in api.views:
     logger.error('View has been deleted')
 else:
     logger.info('View was not deleted')
