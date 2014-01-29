a6eb9258607c94a08089a1c3fb88a5bb79197a7b
Salim Fadhley <sal@stodge.org>
2014-01-29 00:47:52 +0000

    Remove inactive developers from the main PROJECT_AUTHORS.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 PROJECT_ROOT, _ = os.path.split(__file__)
 REVISION = '0.2.17'
 PROJECT_NAME = 'JenkinsAPI'
-PROJECT_AUTHORS = "Salim Fadhley, Ramon van Alteren, Ruslan Lutsenko"
-PROJECT_EMAILS = 'salimfadhley@gmail.com, ramon@vanalteren.nl, ruslan.lutcenko@gmail.com'
+PROJECT_AUTHORS = "Salim Fadhley" # Please see readme.rst for a complete list of contributors
+PROJECT_EMAILS = 'salimfadhley@gmail.com'
 PROJECT_URL = "https://github.com/salimfadhley/jenkinsapi"
 SHORT_DESCRIPTION = 'A Python API for accessing resources on a Jenkins continuous-integration server.'
 
