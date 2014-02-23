783153cf2e9410c55d8e52d095e46e73926b6942
Aleksey Maksimov <ctpeko3a@gmail.com>
2014-02-23 20:01:50 +0800

    Changed Jenkins URL to download LTS version
    Bleeding edge version may not be stable.

--- jenkinsapi_tests/systests/get-jenkins-war.sh
+++ jenkinsapi_tests/systests/get-jenkins-war.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-JENKINS_WAR_URL="http://mirrors.jenkins-ci.org/war/latest/jenkins.war"
+#JENKINS_WAR_URL="http://mirrors.jenkins-ci.org/war/latest/jenkins.war"
+JENKINS_WAR_URL="http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war"
+
 wget $JENKINS_WAR_URL
