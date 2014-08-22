375c5674a0633b8388a0b15d798422b983850af0
ddavydov <ddavydov@twitter.com>
2014-08-22 09:56:12 -0700

    Linter fixes

--- jenkinsapi/utils/krb_requester.py
+++ jenkinsapi/utils/krb_requester.py
@@ -14,7 +14,8 @@ class KrbRequester(Requester):
 
     def __init__(self, ssl_verify=None, baseurl=None, mutual_auth=OPTIONAL):
         """
-        :param ssl_verify: flag indicating if server certificate in HTTPS requests should be verified
+        :param ssl_verify: flag indicating if server certificate in HTTPS requests should be
+                           verified
         :param baseurl: Jenkins' base URL
         :param mutual_auth: type of mutual authentication, use one of REQUIRED, OPTIONAL or DISABLED
                             from requests_kerberos package
@@ -28,7 +29,8 @@ class KrbRequester(Requester):
         self.mutual_auth = mutual_auth
 
     def get_request_dict(self, params=None, data=None, files=None, headers=None):
-      req_dict = super(KrbRequester, self).get_request_dict(params=params, data=data, files=files, headers=headers)
+      req_dict = super(KrbRequester, self).get_request_dict(params=params, data=data, files=files,
+                                                            headers=headers)
       if self.mutual_auth:
          auth = HTTPKerberosAuth(self.mutual_auth)
       else:
