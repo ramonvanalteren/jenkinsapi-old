5999279df8d2c230f6d5d503d1f27748f25df185
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-10-27 09:56:01 -0700

    Removed UTF-8 as a method of decoding the content bytes

--- jenkinsapi/build.py
+++ jenkinsapi/build.py
@@ -414,8 +414,6 @@ class Build(JenkinsBase):
             return content
         elif isinstance(content, bytes):
             return content.decode('ISO-8859-1')
-        elif isinstance(content, bytes):
-            return content.decode('utf-8')
         else:
             raise JenkinsAPIException('Unknown content type for console')
 
