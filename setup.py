67701ef829436c3a1455e9d70cddf5acb54305ff
wiggin15 <wiggin15@yahoo.com>
2016-01-11 17:00:31 +0200

    Update setup.py classifiers
    The classifiers are used by PyPI to recognize Python 3 compatible packages (also by caniusepython3.com and developers).

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Testing',
     ],
 )
