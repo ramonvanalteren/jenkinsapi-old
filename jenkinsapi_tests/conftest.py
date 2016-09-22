b68027230b384a22e36d9f438a798a197360d4c6
Aleksey Maksimov <ctpeko3a@gmail.com>
2016-09-22 22:41:24 +0800

    Fixed launcher

--- jenkinsapi_tests/conftest.py
+++ jenkinsapi_tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 
@@ -9,4 +10,6 @@ logging.basicConfig(
 modules = ['requests.packages.urllib3.connectionpool']
 for module_name in modules:
     logger = logging.getLogger(module_name)
-    logger.setLevel(logging.WARNING)
+    level = logging.WARNING if 'LOG_LEVEL' not in os.environ \
+        else os.environ['LOG_LEVEL'].upper().strip()
+    logger.setLevel(level)
