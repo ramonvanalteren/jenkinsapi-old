f06d8e80dcb589520d4291390226dca2275e6712
Aleksey Maksimov <ctpeko3a@gmail.com>
2016-09-25 21:43:21 +0800

    Converted test_executors to pytest

--- jenkinsapi_tests/unittests/test_executors.py
+++ jenkinsapi_tests/unittests/test_executors.py
@@ -1,328 +1,334 @@
+import pytest
 import mock
-import types
-# To run unittests on python 2.6 please use unittest2 library
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
-
-from jenkinsapi.custom_exceptions import JenkinsAPIException
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.executors import Executors
 from jenkinsapi.executor import Executor
 
 
-class TestExecutors(unittest.TestCase):
+DATAM = {
+    'assignedLabels': [{}],
+    'description': None,
+    'jobs': [],
+    'mode': 'NORMAL',
+    'nodeDescription': 'the master Jenkins node',
+    'nodeName': '',
+    'numExecutors': 2,
+    'overallLoad': {},
+    'primaryView': {'name': 'All', 'url': 'http://localhost:8080/'},
+    'quietingDown': False,
+    'slaveAgentPort': 0,
+    'unlabeledLoad': {},
+    'useCrumbs': False,
+    'useSecurity': False,
+    'views': [
+        {'name': 'All', 'url': 'http://localhost:8080/'},
+        {'name': 'BigMoney', 'url': 'http://localhost:8080/view/BigMoney/'}
+    ]
+}
 
-    DATAM = {
-        'assignedLabels': [{}],
-        'description': None,
-        'jobs': [],
-        'mode': 'NORMAL',
-        'nodeDescription': 'the master Jenkins node',
-        'nodeName': '',
-        'numExecutors': 2,
-        'overallLoad': {},
-        'primaryView': {'name': 'All', 'url': 'http://localhost:8080/'},
-        'quietingDown': False,
-        'slaveAgentPort': 0,
-        'unlabeledLoad': {},
-        'useCrumbs': False,
-        'useSecurity': False,
-        'views': [
-            {'name': 'All', 'url': 'http://localhost:8080/'},
-            {'name': 'BigMoney', 'url': 'http://localhost:8080/view/BigMoney/'}
-        ]
-    }
+DATA0 = {
+    "actions": [
+    ],
+    "displayName": "host0.host.com",
+    "executors": [
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+    ],
+    "icon": "computer.png",
+    "idle": False,
+    "jnlpAgent": True,
+    "launchSupported": False,
+    "loadStatistics": {
 
-    DATA0 = {
-        "actions": [
-        ],
-        "displayName": "host0.host.com",
-        "executors": [
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
-            {},
-            {}
-        ],
-        "icon": "computer.png",
-        "idle": False,
-        "jnlpAgent": True,
-        "launchSupported": False,
-        "loadStatistics": {
-
+    },
+    "manualLaunchAllowed": True,
+    "monitorData": {
+        "hudson.node_monitors.SwapSpaceMonitor": {
+            "availablePhysicalMemory": 8462417920,
+            "availableSwapSpace": 0,
+            "totalPhysicalMemory": 75858042880,
+            "totalSwapSpace": 0
         },
-        "manualLaunchAllowed": True,
-        "monitorData": {
-            "hudson.node_monitors.SwapSpaceMonitor": {
-                "availablePhysicalMemory": 8462417920,
-                "availableSwapSpace": 0,
-                "totalPhysicalMemory": 75858042880,
-                "totalSwapSpace": 0
-            },
-            "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
-            "hudson.node_monitors.ResponseTimeMonitor": {
-                "average": 2
-            },
-            "hudson.node_monitors.TemporarySpaceMonitor": {
-                "path": "/tmp",
-                "size": 430744551424
-            },
-            "hudson.node_monitors.DiskSpaceMonitor": {
-                "path": "/data/jenkins",
-                "size": 1214028627968
-            },
-            "hudson.node_monitors.ClockMonitor": {
-                "diff": 1
-            }
+        "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
+        "hudson.node_monitors.ResponseTimeMonitor": {
+            "average": 2
         },
-        "numExecutors": 8,
-        "offline": False,
-        "offlineCause": None,
-        "offlineCauseReason": "",
-        "oneOffExecutors": [
-            {},
-            {}
-        ],
-        "temporarilyOffline": False
-    }
-
-    DATA1 = {
-        "actions": [
-        ],
-        "displayName": "host1.host.com",
-        "executors": [
-            {},
-            {}
-        ],
-        "icon": "computer.png",
-        "idle": False,
-        "jnlpAgent": True,
-        "launchSupported": False,
-        "loadStatistics": {
+        "hudson.node_monitors.TemporarySpaceMonitor": {
+            "path": "/tmp",
+            "size": 430744551424
         },
-        "manualLaunchAllowed": True,
-        "monitorData": {
-            "hudson.node_monitors.SwapSpaceMonitor": {
-                "availablePhysicalMemory": 8462417920,
-                "availableSwapSpace": 0,
-                "totalPhysicalMemory": 75858042880,
-                "totalSwapSpace": 0
-            },
-            "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
-            "hudson.node_monitors.ResponseTimeMonitor": {
-                "average": 2
-            },
-            "hudson.node_monitors.TemporarySpaceMonitor": {
-                "path": "/tmp",
-                "size": 430744551424
-            },
-            "hudson.node_monitors.DiskSpaceMonitor": {
-                "path": "/data/jenkins",
-                "size": 1214028627968
-            },
-            "hudson.node_monitors.ClockMonitor": {
-                "diff": 1
-            }
+        "hudson.node_monitors.DiskSpaceMonitor": {
+            "path": "/data/jenkins",
+            "size": 1214028627968
         },
-        "numExecutors": 2,
-        "offline": False,
-        "offlineCause": None,
-        "offlineCauseReason": "",
-        "oneOffExecutors": [
-            {},
-            {}
-        ],
-        "temporarilyOffline": False
-    }
+        "hudson.node_monitors.ClockMonitor": {
+            "diff": 1
+        }
+    },
+    "numExecutors": 8,
+    "offline": False,
+    "offlineCause": None,
+    "offlineCauseReason": "",
+    "oneOffExecutors": [
+        {},
+        {}
+    ],
+    "temporarilyOffline": False
+}
 
-    DATA2 = {
-        "actions": [
-        ],
-        "displayName": "host2.host.com",
-        "executors": [
-            {},
-            {},
-            {},
-            {}
-        ],
-        "icon": "computer.png",
-        "idle": False,
-        "jnlpAgent": True,
-        "launchSupported": False,
-        "loadStatistics": {
+DATA1 = {
+    "actions": [
+    ],
+    "displayName": "host1.host.com",
+    "executors": [
+        {},
+        {}
+    ],
+    "icon": "computer.png",
+    "idle": False,
+    "jnlpAgent": True,
+    "launchSupported": False,
+    "loadStatistics": {
+    },
+    "manualLaunchAllowed": True,
+    "monitorData": {
+        "hudson.node_monitors.SwapSpaceMonitor": {
+            "availablePhysicalMemory": 8462417920,
+            "availableSwapSpace": 0,
+            "totalPhysicalMemory": 75858042880,
+            "totalSwapSpace": 0
         },
-        "manualLaunchAllowed": True,
-        "monitorData": {
-            "hudson.node_monitors.SwapSpaceMonitor": {
-                "availablePhysicalMemory": 8462417920,
-                "availableSwapSpace": 0,
-                "totalPhysicalMemory": 75858042880,
-                "totalSwapSpace": 0
-            },
-            "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
-            "hudson.node_monitors.ResponseTimeMonitor": {
-                "average": 2
-            },
-            "hudson.node_monitors.TemporarySpaceMonitor": {
-                "path": "/tmp",
-                "size": 430744551424
-            },
-            "hudson.node_monitors.DiskSpaceMonitor": {
-                "path": "/data/jenkins",
-                "size": 1214028627968
-            },
-            "hudson.node_monitors.ClockMonitor": {
-                "diff": 1
-            }
+        "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
+        "hudson.node_monitors.ResponseTimeMonitor": {
+            "average": 2
         },
-        "numExecutors": 4,
-        "offline": False,
-        "offlineCause": None,
-        "offlineCauseReason": "",
-        "oneOffExecutors": [
-            {},
-            {}
-        ],
-        "temporarilyOffline": False
-    }
-
-    DATA3 = {
-        "actions": [
-        ],
-        "displayName": "host3.host.com",
-        "executors": [
-            {}
-        ],
-        "icon": "computer.png",
-        "idle": False,
-        "jnlpAgent": True,
-        "launchSupported": False,
-        "loadStatistics": {},
-        "manualLaunchAllowed": True,
-        "monitorData": {
-            "hudson.node_monitors.SwapSpaceMonitor": {
-                "availablePhysicalMemory": 8462417920,
-                "availableSwapSpace": 0,
-                "totalPhysicalMemory": 75858042880,
-                "totalSwapSpace": 0
-            },
-            "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
-            "hudson.node_monitors.ResponseTimeMonitor": {
-                "average": 2
-            },
-            "hudson.node_monitors.TemporarySpaceMonitor": {
-                "path": "/tmp",
-                "size": 430744551424
-            },
-            "hudson.node_monitors.DiskSpaceMonitor": {
-                "path": "/data/jenkins",
-                "size": 1214028627968
-            },
-            "hudson.node_monitors.ClockMonitor": {
-                "diff": 1
-            }
+        "hudson.node_monitors.TemporarySpaceMonitor": {
+            "path": "/tmp",
+            "size": 430744551424
         },
-        "numExecutors": 1,
-        "offline": False,
-        "offlineCause": None,
-        "offlineCauseReason": "",
-        "oneOffExecutors": [
-            {},
-            {}
-        ],
-        "temporarilyOffline": False
-    }
-
-    EXEC0 = {
-        "currentExecutable": {
-            "number": 4168,
-            "url": "http://localhost:8080/job/testjob/4168/"
+        "hudson.node_monitors.DiskSpaceMonitor": {
+            "path": "/data/jenkins",
+            "size": 1214028627968
         },
-        "currentWorkUnit": {},
-        "idle": False,
-        "likelyStuck": False,
-        "number": 0,
-        "progress": 48
-    }
+        "hudson.node_monitors.ClockMonitor": {
+            "diff": 1
+        }
+    },
+    "numExecutors": 2,
+    "offline": False,
+    "offlineCause": None,
+    "offlineCauseReason": "",
+    "oneOffExecutors": [
+        {},
+        {}
+    ],
+    "temporarilyOffline": False
+}
 
-    EXEC1 = {
-        "currentExecutable": None,
-        "currentWorkUnit": None,
-        "idle": True,
-        "likelyStuck": False,
-        "number": 0,
-        "progress": -1
-    }
+DATA2 = {
+    "actions": [
+    ],
+    "displayName": "host2.host.com",
+    "executors": [
+        {},
+        {},
+        {},
+        {}
+    ],
+    "icon": "computer.png",
+    "idle": False,
+    "jnlpAgent": True,
+    "launchSupported": False,
+    "loadStatistics": {
+    },
+    "manualLaunchAllowed": True,
+    "monitorData": {
+        "hudson.node_monitors.SwapSpaceMonitor": {
+            "availablePhysicalMemory": 8462417920,
+            "availableSwapSpace": 0,
+            "totalPhysicalMemory": 75858042880,
+            "totalSwapSpace": 0
+        },
+        "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
+        "hudson.node_monitors.ResponseTimeMonitor": {
+            "average": 2
+        },
+        "hudson.node_monitors.TemporarySpaceMonitor": {
+            "path": "/tmp",
+            "size": 430744551424
+        },
+        "hudson.node_monitors.DiskSpaceMonitor": {
+            "path": "/data/jenkins",
+            "size": 1214028627968
+        },
+        "hudson.node_monitors.ClockMonitor": {
+            "diff": 1
+        }
+    },
+    "numExecutors": 4,
+    "offline": False,
+    "offlineCause": None,
+    "offlineCauseReason": "",
+    "oneOffExecutors": [
+        {},
+        {}
+    ],
+    "temporarilyOffline": False
+}
 
-    @mock.patch.object(Jenkins, '_poll')
-    def setUp(self, _poll_jenkins):
-        _poll_jenkins.return_value = self.DATAM
-        self.J = Jenkins('http://localhost:8080')
+DATA3 = {
+    "actions": [
+    ],
+    "displayName": "host3.host.com",
+    "executors": [
+        {}
+    ],
+    "icon": "computer.png",
+    "idle": False,
+    "jnlpAgent": True,
+    "launchSupported": False,
+    "loadStatistics": {},
+    "manualLaunchAllowed": True,
+    "monitorData": {
+        "hudson.node_monitors.SwapSpaceMonitor": {
+            "availablePhysicalMemory": 8462417920,
+            "availableSwapSpace": 0,
+            "totalPhysicalMemory": 75858042880,
+            "totalSwapSpace": 0
+        },
+        "hudson.node_monitors.ArchitectureMonitor": "Linux (amd64)",
+        "hudson.node_monitors.ResponseTimeMonitor": {
+            "average": 2
+        },
+        "hudson.node_monitors.TemporarySpaceMonitor": {
+            "path": "/tmp",
+            "size": 430744551424
+        },
+        "hudson.node_monitors.DiskSpaceMonitor": {
+            "path": "/data/jenkins",
+            "size": 1214028627968
+        },
+        "hudson.node_monitors.ClockMonitor": {
+            "diff": 1
+        }
+    },
+    "numExecutors": 1,
+    "offline": False,
+    "offlineCause": None,
+    "offlineCauseReason": "",
+    "oneOffExecutors": [
+        {},
+        {}
+    ],
+    "temporarilyOffline": False
+}
 
-    def testRepr(self):
-        # Can we produce a repr string for this object
-        assert repr(self.J)
+EXEC0 = {
+    "currentExecutable": {
+        "number": 4168,
+        "url": "http://localhost:8080/job/testjob/4168/"
+    },
+    "currentWorkUnit": {},
+    "idle": False,
+    "likelyStuck": False,
+    "number": 0,
+    "progress": 48
+}
 
-    def testCheckURL(self):
-        self.assertEquals(self.J.baseurl, 'http://localhost:8080')
-
-    @mock.patch.object(Executors, '_poll')
-    @mock.patch.object(Executor, '_poll')
-    def testGetExecutors(self, _poll_executor, _poll_executors):
-        _poll_executor.return_value = self.EXEC0
-        _poll_executors.return_value = self.DATA3
-        exec_info = self.J.get_executors(self.DATA3['displayName'])
-
-        self.assertIsInstance(exec_info, object)
-        self.assertIsInstance(repr(exec_info), str)
-
-        for e in exec_info:
-            self.assertEquals(e.get_progress(), 48, 'Should return 48 %')
-
-    @mock.patch.object(Executors, '_poll')
-    @mock.patch.object(Executor, '_poll')
-    def testis_idle(self, _poll_executor, _poll_executors):
-        _poll_executor.return_value = self.EXEC1
-        _poll_executors.return_value = self.DATA3
-        exec_info = self.J.get_executors('host3.host.com')
-
-        self.assertIsInstance(exec_info, object)
-        for e in exec_info:
-            self.assertEquals(e.get_progress(), -1, 'Should return 48 %')
-            self.assertEquals(e.is_idle(), True, 'Should return True')
-            self.assertEquals(
-                repr(e),
-                '<jenkinsapi.executor.Executor host3.host.com 0>'
-            )
-
-    @mock.patch.object(Executor, '_poll')
-    def test_likely_stuck(self, _poll_executor):
-        _poll_executor.return_value = self.EXEC0
-        baseurl = 'http://localhost:8080/computer/host0.host.com/executors/0'
-        nodename = 'host0.host.com'
-        single_executer = Executor(baseurl, nodename, self.J, '0')
-        self.assertEquals(single_executer.likely_stuck(), False)
-
-    @mock.patch.object(Executor, '_poll')
-    def test_get_current_executable(self, _poll_executor):
-        _poll_executor.return_value = self.EXEC0
-        baseurl = 'http://localhost:8080/computer/host0.host.com/executors/0'
-        nodename = 'host0.host.com'
-        single_executer = Executor(baseurl, nodename, self.J, '0')
-        self.assertEquals(
-            single_executer.get_current_executable()['number'],
-            4168
-        )
-        self.assertEquals(
-            single_executer.get_current_executable()['url'],
-            'http://localhost:8080/job/testjob/4168/'
-        )
+EXEC1 = {
+    "currentExecutable": None,
+    "currentWorkUnit": None,
+    "idle": True,
+    "likelyStuck": False,
+    "number": 0,
+    "progress": -1
+}
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture(scope='function')
+def jenkins(monkeypatch):
+    def fake_poll(cls, tree=None):      # pylint: disable=unused-argument
+        return DATAM
+
+    monkeypatch.setattr(Jenkins, '_poll', fake_poll)
+    return Jenkins('http://localhost:8080')
+
+
+def test_repr(jenkins):
+    # Can we produce a repr string for this object
+    assert repr(jenkins)
+
+
+def test_check_url(jenkins):
+    assert jenkins.baseurl == 'http://localhost:8080'
+
+
+def test_get_executors(jenkins, monkeypatch):
+    def fake_poll_extr(cls, tree=None):  # pylint: disable=unused-argument
+        return EXEC0
+
+    def fake_poll_extrs(cls, tree=None):  # pylint: disable=unused-argument
+        return DATA3
+
+    monkeypatch.setattr(Executor, '_poll', fake_poll_extr)
+    monkeypatch.setattr(Executors, '_poll', fake_poll_extrs)
+
+    exec_info = jenkins.get_executors(DATA3['displayName'])
+
+    assert isinstance(exec_info, object) is True
+    assert isinstance(repr(exec_info), str) is True
+
+    for ex in exec_info:
+        assert ex.get_progress() == 48, 'Should return 48 %'
+
+
+def testis_idle(jenkins, monkeypatch):
+    def fake_poll_extr(cls, tree=None):  # pylint: disable=unused-argument
+        return EXEC1
+
+    def fake_poll_extrs(cls, tree=None):  # pylint: disable=unused-argument
+        return DATA3
+
+    monkeypatch.setattr(Executor, '_poll', fake_poll_extr)
+    monkeypatch.setattr(Executors, '_poll', fake_poll_extrs)
+
+    exec_info = jenkins.get_executors('host3.host.com')
+
+    assert isinstance(exec_info, object) is True
+    for ex in exec_info:
+        assert ex.get_progress() == -1, 'Should return 48 %'
+        assert ex.is_idle() is True, 'Should return True'
+        assert repr(ex) == '<jenkinsapi.executor.Executor host3.host.com 0>'
+
+
+@mock.patch.object(Executor, '_poll')
+def test_likely_stuck(jenkins, monkeypatch):
+    def fake_poll_extr(cls, tree=None):  # pylint: disable=unused-argument
+        return EXEC0
+
+    monkeypatch.setattr(Executor, '_poll', fake_poll_extr)
+
+    baseurl = 'http://localhost:8080/computer/host0.host.com/executors/0'
+    nodename = 'host0.host.com'
+    single_executer = Executor(baseurl, nodename, jenkins, '0')
+    assert single_executer.likely_stuck() is False
+
+
+def test_get_current_executable(jenkins, monkeypatch):
+    def fake_poll_extr(cls, tree=None):  # pylint: disable=unused-argument
+        return EXEC0
+
+    monkeypatch.setattr(Executor, '_poll', fake_poll_extr)
+
+    baseurl = 'http://localhost:8080/computer/host0.host.com/executors/0'
+    nodename = 'host0.host.com'
+    single_executer = Executor(baseurl, nodename, jenkins, '0')
+    assert single_executer.get_current_executable()['number'] == 4168
+    assert single_executer.get_current_executable()['url'] == \
+        'http://localhost:8080/job/testjob/4168/'
