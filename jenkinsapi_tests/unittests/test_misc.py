fefed1d903ead0f5e5ce1eacdd9942027993cc08
Aleksey Maksimov <ctpeko3a@gmail.com>
2016-10-02 23:01:49 +0800

    Converted test_misc

--- jenkinsapi_tests/unittests/test_misc.py
+++ jenkinsapi_tests/unittests/test_misc.py
@@ -1,17 +1,11 @@
-import unittest
 import jenkinsapi
 
 
-class TestMisc(unittest.TestCase):
-
-    def test_jenkinsapi_version(self):
-        """Verify that we can get the jenkinsapi version number from the
-        package's __version__ property.
-        """
-        v = jenkinsapi.__version__
-        parts = [int(x) for x in v.split('.')]
-        for p in parts:
-            assert p >= 0, "Implausible version number: %r" % v
-
-if __name__ == '__main__':
-    unittest.main()
+def test_jenkinsapi_version():
+    """Verify that we can get the jenkinsapi version number from the
+    package's __version__ property.
+    """
+    version = jenkinsapi.__version__
+    parts = [int(x) for x in version.split('.')]
+    for part in parts:
+        assert part >= 0, "Implausible version number: %r" % version
