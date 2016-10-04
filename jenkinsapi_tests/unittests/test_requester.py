ff851aaa9a759979a003ced7e78132c60ea69ba8
Aleksey Maksimov <ctpeko3a@gmail.com>
2016-10-04 22:24:24 +0800

    Converter test_requester.py

--- jenkinsapi_tests/unittests/test_requester.py
+++ jenkinsapi_tests/unittests/test_requester.py
@@ -1,219 +1,254 @@
 from __future__ import print_function
-
-import mock
-# To run unittests on python 2.6 please use unittest2 library
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
-
+import pytest
 import requests
 from jenkinsapi.jenkins import Requester
 from jenkinsapi.custom_exceptions import JenkinsAPIException
 
 
-class TestQueue(unittest.TestCase):
+def test_get_request_dict_auth():
+    req = Requester('foo', 'bar')
 
-    def test_get_request_dict_auth(self):
-        req = Requester('foo', 'bar')
+    req_return = req.get_request_dict(
+        params={},
+        data=None,
+        headers=None
+    )
+    assert isinstance(req_return, dict)
+    assert req_return.get('auth')
+    assert req_return['auth'] == ('foo', 'bar')
 
-        req_return = req.get_request_dict(
+
+def test_get_request_dict_wrong_params():
+    req = Requester('foo', 'bar')
+
+    with pytest.raises(AssertionError) as na:
+        req.get_request_dict(
+            params='wrong',
+            data=None,
+            headers=None
+        )
+    assert "Params must be a dict, got 'wrong'" in str(na.value)
+
+
+def test_get_request_dict_correct_params():
+    req = Requester('foo', 'bar')
+
+    req_return = req.get_request_dict(
+        params={'param': 'value'},
+        data=None,
+        headers=None
+    )
+
+    assert isinstance(req_return, dict)
+    assert req_return.get('params')
+    assert req_return['params'] == {'param': 'value'}
+
+
+def test_get_request_dict_wrong_headers():
+    req = Requester('foo', 'bar')
+
+    with pytest.raises(AssertionError) as na:
+        req.get_request_dict(
             params={},
             data=None,
-            headers=None
+            headers='wrong'
         )
-        self.assertTrue(isinstance(req_return, dict))
-        self.assertTrue(req_return.get('auth'))
-        self.assertTrue(req_return['auth'] == ('foo', 'bar'))
+    assert "headers must be a dict, got 'wrong'" in str(na.value)
 
-    def test_get_request_dict_wrong_params(self):
-        req = Requester('foo', 'bar')
 
-        with self.assertRaises(AssertionError) as na:
-            req.get_request_dict(
-                params='wrong',
-                data=None,
-                headers=None
-            )
-        self.assertTrue(
-            str(na.exception) == "Params must be a dict, got 'wrong'")
+def test_get_request_dict_correct_headers():
+    req = Requester('foo', 'bar')
 
-    def test_get_request_dict_correct_params(self):
-        req = Requester('foo', 'bar')
+    req_return = req.get_request_dict(
+        params={'param': 'value'},
+        data=None,
+        headers={'header': 'value'}
+    )
 
-        req_return = req.get_request_dict(
-            params={'param': 'value'},
-            data=None,
-            headers=None
-        )
+    assert isinstance(req_return, dict)
+    assert req_return.get('headers')
+    assert req_return['headers'] == {'header': 'value'}
 
-        self.assertTrue(isinstance(req_return, dict))
-        self.assertTrue(req_return.get('params'))
-        self.assertTrue(req_return['params'] == {'param': 'value'})
 
-    def test_get_request_dict_wrong_headers(self):
-        req = Requester('foo', 'bar')
+def test_get_request_dict_data_passed():
+    req = Requester('foo', 'bar')
 
-        with self.assertRaises(AssertionError) as na:
-            req.get_request_dict(
-                params={},
-                data=None,
-                headers='wrong'
-            )
-        self.assertTrue(
-            str(na.exception) == "headers must be a dict, got 'wrong'")
+    req_return = req.get_request_dict(
+        params={'param': 'value'},
+        data='some data',
+        headers={'header': 'value'}
+    )
 
-    def test_get_request_dict_correct_headers(self):
-        req = Requester('foo', 'bar')
+    assert isinstance(req_return, dict)
+    assert req_return.get('data')
+    assert req_return['data'] == 'some data'
 
-        req_return = req.get_request_dict(
-            params={'param': 'value'},
-            data=None,
-            headers={'header': 'value'}
-        )
 
-        self.assertTrue(isinstance(req_return, dict))
-        self.assertTrue(req_return.get('headers'))
-        self.assertTrue(req_return['headers'] == {'header': 'value'})
+def test_get_request_dict_data_not_passed():
+    req = Requester('foo', 'bar')
 
-    def test_get_request_dict_data_passed(self):
-        req = Requester('foo', 'bar')
+    req_return = req.get_request_dict(
+        params={'param': 'value'},
+        data=None,
+        headers={'header': 'value'}
+    )
 
-        req_return = req.get_request_dict(
-            params={'param': 'value'},
-            data='some data',
-            headers={'header': 'value'}
-        )
+    assert isinstance(req_return, dict)
+    assert req_return.get('data') is None
 
-        self.assertTrue(isinstance(req_return, dict))
-        # print(req_return.get('data'))
-        self.assertTrue(req_return.get('data'))
-        self.assertTrue(req_return['data'] == 'some data')
 
-    def test_get_request_dict_data_not_passed(self):
-        req = Requester('foo', 'bar')
+def test_get_url_get(monkeypatch):
+    def fake_get(*args, **kwargs):  # pylint: disable=unused-argument
+        return 'SUCCESS'
 
-        req_return = req.get_request_dict(
-            params={'param': 'value'},
-            data=None,
-            headers={'header': 'value'}
-        )
+    monkeypatch.setattr(requests, 'get', fake_get)
 
-        self.assertTrue(isinstance(req_return, dict))
-        self.assertFalse(req_return.get('data'))
+    req = Requester('foo', 'bar')
+    response = req.get_url(
+        'http://dummy',
+        params={'param': 'value'},
+        headers=None)
 
-    @mock.patch.object(requests, 'get')
-    def test_get_url_get(self, _get):
-        _get.return_value = 'SUCCESS'
-        req = Requester('foo', 'bar')
-        response = req.get_url(
-            'http://dummy',
-            params={'param': 'value'},
-            headers=None)
-        self.assertTrue(response == 'SUCCESS')
+    assert response == 'SUCCESS'
 
-    @mock.patch.object(requests, 'post')
-    def test_get_url_post(self, _post):
-        _post.return_value = 'SUCCESS'
-        req = Requester('foo', 'bar')
-        response = req.post_url(
-            'http://dummy',
-            params={'param': 'value'},
-            headers=None)
-        self.assertTrue(response == 'SUCCESS')
 
-    @mock.patch.object(requests, 'post')
-    def test_post_xml_and_confirm_status_empty_xml(self, _post):
-        _post.return_value = mock.Mock()
-        req = Requester('foo', 'bar')
-        with self.assertRaises(AssertionError) as ae:
-            req.post_xml_and_confirm_status(
-                url='http://dummy',
-                params={'param': 'value'},
-                data=None
-            )
+def test_get_url_post(monkeypatch):
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return 'SUCCESS'
 
-    @mock.patch.object(requests, 'post')
-    def test_post_xml_and_confirm_status_some_xml(self, _post):
-        response = requests.Response()
-        response.status_code = 200
-        _post.return_value = response
-        req = Requester('foo', 'bar')
-        ret = req.post_xml_and_confirm_status(
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    req = Requester('foo', 'bar')
+    response = req.post_url(
+        'http://dummy',
+        params={'param': 'value'},
+        headers=None)
+
+    assert response == 'SUCCESS'
+
+
+def test_post_xml_empty_xml(monkeypatch):
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return 'SUCCESS'
+
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    req = Requester('foo', 'bar')
+    with pytest.raises(AssertionError):
+        req.post_xml_and_confirm_status(
             url='http://dummy',
             params={'param': 'value'},
-            data='<xml/>'
+            data=None
         )
-        self.assertTrue(isinstance(ret, requests.Response))
-
-    @mock.patch.object(requests, 'post')
-    def test_post_and_confirm_status_empty_data(self, _post):
-        mock_response = mock.Mock()
-        _post.return_value = mock_response
-        req = Requester('foo', 'bar')
-        with self.assertRaises(AssertionError) as ae:
-            req.post_and_confirm_status(
-                url='http://dummy',
-                params={'param': 'value'},
-                data=None
-            )
 
 
-    @mock.patch.object(requests, 'post')
-    def test_post_and_confirm_status_some_data(self, _post):
-        response = requests.Response()
-        response.status_code = 200
-        _post.return_value = response
-        req = Requester('foo', 'bar')
-        ret = req.post_and_confirm_status(
+def test_post_xml_and_confirm_status_some_xml(monkeypatch):
+    class FakeResponse(requests.Response):
+        def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+            self.status_code = 200
+
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return FakeResponse()
+
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    req = Requester('foo', 'bar')
+    ret = req.post_xml_and_confirm_status(
+        url='http://dummy',
+        params={'param': 'value'},
+        data='<xml/>'
+    )
+    assert isinstance(ret, requests.Response)
+
+
+def test_post_and_confirm_status_empty_data(monkeypatch):
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return 'SUCCESS'
+
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    req = Requester('foo', 'bar')
+    with pytest.raises(AssertionError):
+        req.post_and_confirm_status(
+            url='http://dummy',
+            params={'param': 'value'},
+            data=None
+        )
+
+
+def test_post_and_confirm_status_some_data(monkeypatch):
+    class FakeResponse(requests.Response):
+        def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+            self.status_code = 200
+
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return FakeResponse()
+
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    req = Requester('foo', 'bar')
+    ret = req.post_and_confirm_status(
+        url='http://dummy',
+        params={'param': 'value'},
+        data='some data'
+    )
+    assert isinstance(ret, requests.Response)
+
+
+def test_post_and_confirm_status_bad_result(monkeypatch):
+    class FakeResponse(object):
+        def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+            self.status_code = 500
+            self.url = 'http://dummy'
+            self.text = 'something'
+
+    def fake_post(*args, **kwargs):  # pylint: disable=unused-argument
+        return FakeResponse()
+
+    monkeypatch.setattr(requests, 'post', fake_post)
+
+    req = Requester('foo', 'bar')
+    with pytest.raises(JenkinsAPIException):
+        req.post_and_confirm_status(
             url='http://dummy',
             params={'param': 'value'},
             data='some data'
         )
-        self.assertTrue(isinstance(ret, requests.Response))
 
-    @mock.patch.object(requests, 'post')
-    def test_post_and_confirm_status_bad_result(self, _post):
-        response = requests.Response()
-        response.status_code = 500
-        _post.return_value = response
 
-        req = Requester('foo', 'bar')
-        with self.assertRaises(JenkinsAPIException) as ae:
-            req.post_and_confirm_status(
-                url='http://dummy',
-                params={'param': 'value'},
-                data='some data'
-            )
+def test_get_and_confirm_status(monkeypatch):
+    class FakeResponse(requests.Response):
+        def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+            self.status_code = 200
 
-        self.assertIsInstance(ae.exception, JenkinsAPIException)
+    def fake_get(*args, **kwargs):  # pylint: disable=unused-argument
+        return FakeResponse()
 
-    @mock.patch.object(requests, 'get')
-    def test_get_and_confirm_status(self, _get):
-        response = requests.Response()
-        response.status_code = 200
-        _get.return_value = response
-        req = Requester('foo', 'bar')
-        ret = req.get_and_confirm_status(
+    monkeypatch.setattr(requests, 'get', fake_get)
+
+    req = Requester('foo', 'bar')
+    ret = req.get_and_confirm_status(
+        url='http://dummy',
+        params={'param': 'value'}
+    )
+    assert isinstance(ret, requests.Response)
+
+
+def test_get_and_confirm_status_bad_result(monkeypatch):
+    class FakeResponse(object):
+        def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+            self.status_code = 500
+            self.url = 'http://dummy'
+            self.text = 'something'
+
+    def fake_get(*args, **kwargs):  # pylint: disable=unused-argument
+        return FakeResponse()
+
+    monkeypatch.setattr(requests, 'get', fake_get)
+
+    req = Requester('foo', 'bar', baseurl='http://dummy')
+    with pytest.raises(JenkinsAPIException):
+        req.get_and_confirm_status(
             url='http://dummy',
             params={'param': 'value'}
         )
-        self.assertTrue(isinstance(ret, requests.Response))
-
-    @mock.patch.object(requests, 'get')
-    def test_get_and_confirm_status_bad_result(self, _get):
-        response = requests.Response()
-        response.status_code = 500
-        _get.return_value = response
-
-        req = Requester('foo', 'bar', baseurl='http://dummy')
-        with self.assertRaises(JenkinsAPIException) as ae:
-            req.get_and_confirm_status(
-                url='http://dummy',
-                params={'param': 'value'}
-            )
-
-        self.assertIsInstance(ae.exception, JenkinsAPIException)
-
-if __name__ == "__main__":
-    unittest.main()
