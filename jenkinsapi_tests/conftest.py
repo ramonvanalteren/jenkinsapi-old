cdd3f90e4ce5122db24e31038aeee098357313a4
Aleksey Maksimov <aleksey.maksimov@sc.com>
2017-12-09 22:48:45 +0800

    disable logging from requests

--- jenkinsapi_tests/conftest.py
+++ jenkinsapi_tests/conftest.py
@@ -7,9 +7,16 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-modules = ['requests.packages.urllib3.connectionpool']
+level = logging.WARNING if 'LOG_LEVEL' not in os.environ \
+    else os.environ['LOG_LEVEL'].upper().strip()
+
+modules = [
+    'requests.packages.urllib3.connectionpool',
+    'requests',
+    'urllib3',
+    'urllib3.connectionpool'
+]
+
 for module_name in modules:
     logger = logging.getLogger(module_name)
-    level = logging.WARNING if 'LOG_LEVEL' not in os.environ \
-        else os.environ['LOG_LEVEL'].upper().strip()
     logger.setLevel(level)
