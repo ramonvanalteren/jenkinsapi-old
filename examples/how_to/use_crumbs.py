369dc54a8d5bb1f4e985c647378b9e1e62c26961
Daniel Carrington <daniel.carrington@hpe.com>
2018-01-24 12:45:07 -0600

    Fix arguments passed in how_to/use_crumbs.py

--- examples/how_to/use_crumbs.py
+++ examples/how_to/use_crumbs.py
@@ -4,10 +4,10 @@ Example of using CrumbRequester - when CSRF protection is enable in Jenkins
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.utils.crumb_requester import CrumbRequester
 
-jenkins = Jenkins('http://localhost:8080', user='admin', password='password',
+jenkins = Jenkins('http://localhost:8080', username='admin', password='password',
                   requester=CrumbRequester(
-                       'http://localhost:8080',
-                       user='admin',
+                       baseurl='http://localhost:8080',
+                       username='admin',
                        password='password'
                   ))
 
