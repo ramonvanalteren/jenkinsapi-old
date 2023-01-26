b1af616c02457dab082403e63c43186f340034f3
Aleksey Maksimov <ctpeko3a@gmail.com>
2023-01-26 21:19:14 +0800

    Bump version

--- jenkinsapi/__init__.py
+++ jenkinsapi/__init__.py
@@ -92,7 +92,7 @@ __all__ = [
 ]
 __docformat__ = "epytext"
 # In case of jenkinsapi is not installed in 'develop' mode
-__version__ = "0.3.12"
+__version__ = "0.3.13"
 try:
     import pkg_resources
 
